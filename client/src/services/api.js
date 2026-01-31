b14351add0fb3dd796a4df7445d0810badd2f970
PriyanshiNmeher <167795594+PriyanshiNmeher@users.noreply.github.com>
2026-01-31 19:22:59 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://task-management-uujy.onrender.com/';
+const API_URL = 'https://task-management-uujy.onrender.com/api/tasks';
 
 const getAuthHeader = () => {
     const token = localStorage.getItem('token');
